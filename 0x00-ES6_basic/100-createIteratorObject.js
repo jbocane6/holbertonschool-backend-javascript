f4e3de84a065bb77ae4152381eb53369eadaf4cb
Juan Bocanegra <jbocane6@gmail.com>
2022-04-12 01:25:05 -0500

    [UPDATE] file 100 v.2

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
 export default function createIteratorObject(report) {
-	const tmpArray = [];
+  const tmpArray = [];
   for (const idx of Object.keys(report.allEmployees)) {
-		for (const idx2 of Object.values(report.allEmployees[idx])) {
-			tmpArray.push(idx2);
-		}
-	}
-	return Object.values(tmpArray);
+    for (const idx2 of Object.values(report.allEmployees[idx])) {
+      tmpArray.push(idx2);
+    }
+  }
+  return Object.values(tmpArray);
 }

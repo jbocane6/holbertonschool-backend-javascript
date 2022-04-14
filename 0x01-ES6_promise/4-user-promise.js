121c722508d1c72887a8382168278f35a02a1da5
Juan Bocanegra <jbocane6@gmail.com>
2022-04-13 22:52:13 -0500

    [UPDATE] file 4 v.1

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function signUpUser(firstName, lastName) {
+    return new Promise(resolve => {
+        resolve({
+            firstName: firstName,
+            lastName: lastName,
+          })
+    })
+}

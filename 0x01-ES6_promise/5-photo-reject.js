3d6f7439c4b9ef14b9f6cab8fccc467d7ea9196d
Juan Bocanegra <jbocane6@gmail.com>
2022-04-13 23:06:04 -0500

    [UPDATE] file 5 v.4 what it does that ) there?

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default function uploadPhoto(fileName) {
   return new Promise((resolve, reject) => {
-    reject(new Error(`${fileName} cannot be processed)`));
+    reject(new Error(`${fileName} cannot be processed`));
   });
 }

18ad19fc0fcb7d4ab39bbd65c7e6e130ed6a7d1f
Juan Bocanegra <jbocane6@gmail.com>
2022-04-14 22:28:33 -0500

    [UPDATE] file 4 v.2 eslint

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,4 +1,6 @@
-import Currency from "./3-currency";
+/* eslint-disable */
+import Currency from './3-currency';
+
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
